36de916149a2dbb7926de0e13658ede6845d84cb
Oleksandra_Dmytrenko <oleksandra_dmytrenko1@epam.com>
2017-08-07 17:18:14 +0300

    Проапдейтила метод get(index) в листе, добавила тесты.

--- src/main/java/lists/MyLinkedList.java
+++ src/main/java/lists/MyLinkedList.java
@@ -5,21 +5,43 @@ package lists;
  */
 public class MyLinkedList {
 
-    ListElement first;
-    ListElement last;
+    private ListElement first;
+    private ListElement last;
     int size = 0;
 
-    public void add(String content){
+    public void add(String content) {
         ListElement element = new ListElement(content);
-        if (last != null){
+        if (last != null) {
+            //элемент второй или последующий.
+            //Таких элементов будет больше, чем первых, поэтому это условие лучше чтоб шло первее.
             element.setPrev(last);
             last.setNext(element);
             last = element;
-        }else {
+        } else {
+            //элемент первый и единственный
             first = element;
             last = element;
         }
-        size ++ ;
+        size++;
+    }
+
+    /*
+    На данный момент в самом плохом случае (при поиске последнего элемента) придется пройти по всем элементам в списке.
+    Этот метод можно улучшить, определив, в какой части списка находится необходимый элемент.
+    Если в начале - так и оставить.
+    Если в конце - идти к нужному элементу с конца списка, от last
+     */
+    public String get(int index) {
+        if (index >= 0 && index < size) {
+            ListElement searchedElement = first;
+            for (int i = 0; i < index; i++) {
+                //дойдем до нужного элемента, после чего возьмем его значение
+                searchedElement = first.getNext();
+            }
+            return searchedElement.getValue();
+        } else {
+            throw new IndexOutOfBoundsException("Index is invalid, please input index that will be a positive value, less than " + size);
+        }
     }
 
     public ListElement getFirst() {
@@ -28,13 +50,5 @@ public class MyLinkedList {
 
     public ListElement getLast() {
         return last;
-    }
-
-    public ListElement get(int index){
-        ListElement el = null;
-        for (int i = 0; i < index; i++) {
-             el = first.getNext();
-        }
-        return el;
     }
 }
--- src/test/java/lists/MyLinkedListTest.java
+++ src/test/java/lists/MyLinkedListTest.java
@@ -10,11 +10,16 @@ import static org.junit.Assert.assertEquals;
 public class MyLinkedListTest {
 
     private static final String ПЕРВЫЙ_ЭЛЕМЕНТ = "Первый элемент";
-    private static final String SECOND_ELEMENT = "SecondElement";
-    public static final String THIRD_ELEMENT = "ThirdElement";
+    private static final String SECOND_ELEMENT = "Второй элемент";
+    public static final String THIRD_ELEMENT = "Третий элемент";
 
+    /*
+     * Этот кейс особенный поскольку
+     * в списке будет только один элемент, который одновременно будет как первым, так и последним
+     * @throws Exception
+     */
     @Test
-    public void add1() throws Exception {
+    public void add1Element() throws Exception {
         MyLinkedList list = new MyLinkedList();
         list.add(ПЕРВЫЙ_ЭЛЕМЕНТ);
         ListElement expected = new ListElement(ПЕРВЫЙ_ЭЛЕМЕНТ);
@@ -22,8 +27,13 @@ public class MyLinkedListTest {
         assertEquals(1, list.size);
     }
 
+    /*
+     * Этот кейс особенный поскольку
+     * в списке будет 2 элемента, представляющие 2 разные группы: первый и последний
+     * @throws Exception
+     */
     @Test
-    public void add2() throws Exception {
+    public void add2Elements() throws Exception {
         //GIVEN
         MyLinkedList list = new MyLinkedList();
         ListElement expected1 = new ListElement(ПЕРВЫЙ_ЭЛЕМЕНТ);
@@ -40,8 +50,16 @@ public class MyLinkedListTest {
         assertEquals(2, list.size);
     }
 
+    /*
+     * Этот кейс особенный поскольку
+     * в списке будет 3 элемента, представляющие 3 разные группы: первый, средний и последний.
+     * Написав этот кейс, мы покрыли все группы элементов, которые могут быть в списке данной реализации.
+     * Этот тест по сути включает в себя второй тест кейс. Написав его можно удалить второй, но на всякий случай можно и оставить :)
+     * Это тест граничных условий.
+     * @throws Exception
+     */
     @Test
-    public void add3() throws Exception {
+    public void add3Elements() throws Exception {
         //GIVEN
         MyLinkedList list = new MyLinkedList();
         ListElement expected1 = new ListElement(ПЕРВЫЙ_ЭЛЕМЕНТ);
@@ -56,8 +74,90 @@ public class MyLinkedListTest {
         list.add(SECOND_ELEMENT);
         list.add(THIRD_ELEMENT);
         //THEN
-        assertEquals(expected1, list.getFirst()); // за счет рекурсивности в equals методе, мы проверяем все элементы, проверяя первый.
+        // за счет рекурсивности в equals методе, мы проверяем все элементы, проверяя первый.
+        // минусом использования сравнения через equals(object) является одновременно тестирование самого сравнения,
+        // а не только работы метода добавления элемента
+        assertEquals(expected1, list.getFirst());
         assertEquals(expected3.getValue(), list.getLast().getValue());
         assertEquals(3, list.size);
+    }
+
+    /*
+     * Этот кейс особенный поскольку
+     * при работе метода get(index) программа ни разу не зайдет в цикл поиска элемента.
+     */
+    @Test
+    public void getFirstElementOutOfOne(){
+        //GIVEN - list out of one element
+        MyLinkedList list = new MyLinkedList();
+        list.add(ПЕРВЫЙ_ЭЛЕМЕНТ);
+
+        //WHEN
+        String searchedElementValue = list.get(0);
+
+        //THEN
+        assertEquals(ПЕРВЫЙ_ЭЛЕМЕНТ, searchedElementValue);
+    }
+
+    /*
+     * Этот кейс особенный поскольку
+     * при работе метода get(index) программа зайдет в цикл поиска элемента.
+     */
+    @Test
+    public void getSecondElementOutOfTwo(){
+        //GIVEN - list out of two elements
+        MyLinkedList list = new MyLinkedList();
+        list.add(ПЕРВЫЙ_ЭЛЕМЕНТ);
+        list.add(SECOND_ELEMENT);
+
+        //WHEN
+        String searchedElementValue = list.get(1);
+
+        //THEN
+        assertEquals(SECOND_ELEMENT, searchedElementValue);
+    }
+
+    /*
+     * Проверим, что программа обрабатывает исключительную ситуацию с запросом элемента из спискас отрицательным индексом
+     * Хорошей практикой евляется запись названия теста, которая включает желаемый результат
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowExceptionWhenGetNegativeIndex(){
+        //GIVEN - list out of one element from where it's possible to get something
+        MyLinkedList list = new MyLinkedList();
+        list.add(ПЕРВЫЙ_ЭЛЕМЕНТ);
+
+        //WHEN - попытаемся получить первое значение из группы (класса эквивалентности) отрицательных значений.
+        list.get(-1);
+    }
+
+    /*
+     * Проверим, что программа обрабатывает исключительную ситуацию с запросом элемента из списка с индексом, превышающим количество элементов в списке
+     * Хорошей практикой евляется запись названия теста, которая включает желаемый результат
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowExceptionWhenGetElementWithIndexGreaterThanPresent(){
+        //GIVEN - list out of one element from where it's possible to get something
+        MyLinkedList list = new MyLinkedList();
+        list.add(ПЕРВЫЙ_ЭЛЕМЕНТ);
+
+        //WHEN - попытаемся получить второй элемент, в то время, как в списке только один
+        list.get(1);
+    }
+
+    /*
+     * Проверим, что программа обрабатывает исключительную ситуацию с запросом элемента из списка, в котором нет элементов.
+     * Этот тест есть частным случаем теста shouldThrowExceptionWhenGetElementWithIndexGreaterThanPresent,
+     * поскольку в списке 0 элементов а просим мы первый.
+     * Но такой кейс лучше написать, чем сомневаться, как себя поведет программа в граничной ситуации.
+     * Хорошей практикой евляется запись названия теста, которая включает желаемый результат
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowExceptionWhenGetElementInEmptyNonNullList(){
+        //GIVEN - список без элементов. По умолдчанию поле size инициализировано нулем
+        MyLinkedList list = new MyLinkedList();
+
+        //WHEN - попытаемся получить первое значение из группы (класса эквивалентности) отрицательных значений.
+        list.get(0);
     }
 }
